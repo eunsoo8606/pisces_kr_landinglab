831484cdfa34b277250213f85dbb8de83017bde8
eunsoo8606 <eunsoo8606@gmail.com>
2026-06-16 01:23:21 +0900

    fix: favicon 캐시 버스터 적용 및 SEO 메타 태그 최적화

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
     res.render('index', {
         title: '물고기자리 - 프리미엄 숙성회',
-        branding: 'PISCES since 2002'
+        branding: 'PISCES since 2002',
+        metaDescription: '칼 없는 주방, 주방장 없는 횟집! 물고기자리에서 성공적인 프리미엄 숙성회 프랜차이즈 창업을 시작하세요. 20여 년의 노하우로 최상급 활선어 숙성회를 무한리필로 제공합니다.',
+        metaKeywords: '물고기자리, 숙성회, 무한리필, 활어회, 프랜차이즈, 창업, 횟집, 소자본창업, 일식창업'
     });
 });
 
@@ -43,7 +45,9 @@ app.get(['/menu', '/menu/:category'], (req, res) => {
     res.render('menu', {
         title: '메뉴소개 | 물고기자리 - 프리미엄 숙성회',
         branding: 'PISCES since 2002',
-        activeTab: activeTab
+        activeTab: activeTab,
+        metaDescription: '물고기자리의 대표 메뉴를 소개합니다. 엄선된 최고급 원어로 오랜 시간 정성껏 빚어내는 프리미엄 숙성회 모듬부터 다채로운 사이드 메뉴까지 경험해 보세요.',
+        metaKeywords: '물고기자리 메뉴, 숙성회 코스, 모듬회, 매운탕, 초밥, 횟집 메뉴, 모듬숙성회'
     });
 });
 
@@ -56,7 +60,9 @@ app.use('/franchise', (req, res) => {
 app.get('/brand/about', (req, res) => {
     res.render('brand/about', {
         title: '브랜드스토리 | 물고기자리 - 프리미엄 숙성회',
-        branding: 'PISCES since 2002'
+        branding: 'PISCES since 2002',
+        metaDescription: 'since 2002 물고기자리의 철학과 역사. 왜 물고기자리 숙성회인가? 최고의 맛을 향한 타협 없는 고집과 가치를 지켜나가는 브랜드 스토리입니다.',
+        metaKeywords: '물고기자리 스토리, 숙성회 철학, 횟집 역사, 브랜드스토리, 물고기자리 브랜드'
     });
 });
 
@@ -77,7 +83,9 @@ app.get(['/community', '/community/:category'], (req, res) => {
     res.render('community', {
         title: '커뮤니티 | 물고기자리 - 프리미엄 숙성회',
         branding: 'PISCES since 2002',
-        activeTab: activeTab
+        activeTab: activeTab,
+        metaDescription: '물고기자리 고객 센터 및 소통 공간. 공지사항, 자주 묻는 질문(FAQ) 안내와 함께 서비스 불편사항 접수(고객의 소리), 가맹 및 비즈니스 제휴 문의를 제공합니다.',
+        metaKeywords: '물고기자리 고객센터, 가맹문의, 제휴문의, 공지사항, FAQ, 고객의소리, 불편접수'
     });
 });
 
